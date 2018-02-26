39bf31e75b44172e7f335e1d754007178aecbb60
KyleS22 <25314187+KyleS22@users.noreply.github.com>
2018-02-25 19:37:24 -0600

    Changed quadratic sieve from a class

--- kcryptolearn/quadratic_sieve.py
+++ kcryptolearn/quadratic_sieve.py
@@ -6,24 +6,23 @@ Author: Kyle Seidenthal
 from sympy import sieve
 
 
-class QuadraticSieve:
+def get_factor_base(B):
+    """
+    Returns a list of primes less than B
+    :param B: The smoothness bound
+    :return: The primes less than B
+    """
+    return [p for p in sieve.primerange(2, B)]
 
-    def get_factor_base(self, B):
-        """
-        Returns a list of primes less than B
-        :param B: The smoothness bound
-        :return: The primes less than B
-        """
-        return [p for p in sieve.primerange(2, B)]
 
-    def relation_function(self, T,  N):
-        """
-        Returns the value of the function T^2 - N
-        :param T: The input to the function
-        :param N: The value to use for N
-        :return: T^2 - N
-        """
-        return T**2 - N
+def relation_function(T,  N):
+    """
+    Returns the value of the function T^2 - N
+    :param T: The input to the function
+    :param N: The value to use for N
+    :return: T^2 - N
+    """
+    return T**2 - N
 
 
 
--- tests/test_quadraticSieve.py
+++ tests/test_quadraticSieve.py
@@ -4,13 +4,12 @@
 Author: Kyle Seidenthal
 """
 from unittest import TestCase
-from kcryptolearn.quadratic_sieve import QuadraticSieve
+import kcryptolearn.quadratic_sieve as qs
 
 class TestQuadraticSieve(TestCase):
 
     def test_get_factor_base(self):
-        sieve = QuadraticSieve()
-        factor_base = sieve.get_factor_base(10)
+        factor_base = qs.get_factor_base(10)
         expected = [2, 3, 5, 7]
 
         self.assertListEqual(factor_base, expected)
